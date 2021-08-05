275f1d9f6362b374656a6d788c1764f402f32817
Victor Rodrigues <contato@victormkt.com.br>
2021-08-05 13:13:53 -0300

    pequenos ajustes

--- js/main.js
+++ js/main.js
@@ -4,13 +4,16 @@ $('.owl-carousel').owlCarousel({
     nav:false,
     responsive:{
         0:{
+            items:1
+        },
+        350:{
             items:2
         },
-        600:{
-            items:4
+        700:{
+            items:5
         },
         1000:{
-            items:7
+            items:8
         }
     }
 })

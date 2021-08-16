3b01a62dd8297ebd0f9125a14f4eb2ea557c867e
Victor Rodrigues <contato@victormkt.com.br>
2021-08-16 19:19:07 -0300

    ajuste na rolagem do carrosel

--- js/main.js
+++ js/main.js
@@ -1,19 +1,21 @@
 $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:false,
-    responsive:{
-        0:{
-            items:1
+    loop: true,
+    margin: 10,
+    nav: false,
+    lazyLoad: true,
+    stagePadding: 100,
+    responsive: {
+        0: {
+            items: 1
         },
-        350:{
-            items:2
+        350: {
+            items: 2
         },
-        700:{
-            items:5
+        700: {
+            items: 5
         },
-        1000:{
-            items:8
+        1000: {
+            items: 8
         }
     }
 })
